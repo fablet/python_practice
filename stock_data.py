9f40f01181076983e6a34939a3a5a6312bd28c9c
fablet <fable@raintechpdx.com>
2014-11-05 20:01:03 -0800

    univeralization of stock_data

--- stock_data.py
+++ stock_data.py
@@ -8,28 +8,31 @@ stock_data = [
     ["2014-06-03", "MSFT", 32.53],
     ["2014-06-04", "MSFT", 40.71],
 ]
+INDEX_DATE = 0
+INDEX_SYMBOL = 1
+INDEX_AMOUNT = 2
 
 
 #create a list of unique stocks to later be used as the keys
 #for the dictionary of stock tickers per stock
-def stock_keys(stock_list):
+def unique_keys(data_list, index):
     unique = []
-    for stock in stock_list:
-        if stock[1] not in unique:
-            unique.append(stock[1])
+    for data in data_list:
+        if data[index] not in unique:
+            unique.append(data[index])
     return unique
 
 
 #compile the list of stocks into a dictionary with a list of relevant
 #stock information associated with each stock key
-def stock_groups(stock_list):
+def stock_groups(stock_list, sorting_key, value1, value2):
     stock_dict = {}
     stocks_per_key = []
-    key_list = stock_keys(stock_list)  # get the list of individual stocks
+    key_list = unique_keys(stock_list, sorting_key)  # get the list of individual stocks
     for key in key_list:
         for i in range(0, len(stock_list)):
-            if stock_list[i][1] == key:
-                stock_value = [stock_list[i][0], stock_list[i][2]]
+            if stock_list[i][sorting_key] == key:
+                stock_value = [stock_list[i][value1], stock_list[i][value2]]
                 stocks_per_key.append(stock_value)
         stock_dict[key] = stocks_per_key
         stocks_per_key = []
@@ -45,5 +48,7 @@ def print_stocks(stock_dict):
 
 
 #main program calls
-my_stocks = stock_groups(stock_data)
-print_stocks(my_stocks)
+stocks_by_symbol = stock_groups(stock_data, INDEX_SYMBOL, INDEX_DATE, INDEX_AMOUNT)
+stocks_by_date = stock_groups(stock_data, INDEX_DATE, INDEX_SYMBOL, INDEX_AMOUNT)
+print_stocks(stocks_by_symbol)
+print_stocks(stocks_by_date)
